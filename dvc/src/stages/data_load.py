37d080e3371246fd75353babc995c6d0ecb658c5
soujanyavatt <soujanyanaidu25@gmail.com>
2022-06-01 18:12:02 +0530

    commit

---
+++ dvc/src/stages/data_load.py
@@ -0,0 +1,30 @@
+from ast import arg
+import pandas as pd
+from sklearn.datasets import load_iris
+from typing import Text
+import yaml
+import argparse
+
+
+def data_load(config_path:Text) -> None:    
+
+    with open("params.yaml") as conf_file:
+        config = yaml.safe_load(conf_file)
+    
+
+    data = load_iris(as_frame=True)
+    dataset = data.frame
+
+    dataset.columns = [colname.strip(' (cm)').replace(' ', '_') for colname in dataset.columns.tolist()]
+    dataset.to_csv(config['data_load']['dataset_csv'],index=False)
+
+    print("Data Load completed")
+
+
+if __name__ =='__main__':
+
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config',dest='config',required=True)
+    args = args_parser.parse_args()
+
+    data_load(config_path=args.config)
